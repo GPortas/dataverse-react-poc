1ca20ffcf4886b370e80f35fcdbacccb4802fc83
GPortas <hey@gportas.me>
2022-10-03 11:49:24 +0100

    Added: initial code base with simple browser router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/new/' element={<HomePage />} />
+        <Route path='/new/datasets' element={<DatasetsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+// Home page
+const HomePage = () => {
+  return <div style={{padding: 30}}>
+    <h1>Hello world. This is a React Application test</h1>
+    <p><Link to="/dataverseuser.xhtml" reloadDocument={true}>Go to dataverse user page</Link></p>
+    <p><Link to="/new/datasets">Go to datasets section</Link></p>
+  </div>
+}
+
+// Sample Page
+const DatasetsPage = () => {
+  return <div style={{padding: 30}}>
+    <h1>Datasets section</h1>
+  </div>
 }
 
 export default App;
